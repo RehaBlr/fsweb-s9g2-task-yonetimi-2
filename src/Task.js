208fb7c1d805a09bd45f97d450de02d8137707ec
Reha <134259610+RehaBlr@users.noreply.github.com>
2023-07-25 22:50:03 +0300

    done all project

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,41 @@
 import React from "react";
+import { differenceInDays, formatDistanceToNow } from "date-fns";
+
+import { tr as trDili } from "date-fns/locale";
 
 const Task = ({ taskObj, onComplete }) => {
+  const diffInDays = differenceInDays(new Date(taskObj.deadline), new Date());
+
+  const distaceToNow = formatDistanceToNow(new Date(taskObj.deadline), {
+    addSuffix: true,
+    locale: trDili,
+  });
 
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+    <div className="task p-6 bg-white rounded leading-6 mt-4 shadow-md">
+      <h3 className="text-lg text-[#c8781a]">{taskObj.title}</h3>
+      <div className="deadline ">
+        son teslim:
+        <span className={diffInDays <= 3 ? "bg-less" : "bg-more"}>
+          {distaceToNow}
+        </span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button
+          className="block py-2 px-3 ml-auto bg-[#fecc91] shadow-md rounded border-0 cursor-pointer "
+          onClick={() => onComplete(taskObj.id)}
+        >
+          Tamamlandı
+        </button>
+      )}
     </div>
   );
 };
